7dfb502a77bd4c53ab3429dd25179667369a82f9
avbelovolov <avbelovolov@gmail.com>
2023-12-04 23:13:02 +0500

    Bottom navigation

--- app/src/main/java/com/example/intersvyaztestapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/intersvyaztestapp/ui/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.intersvyaztestapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.intersvyaztestapp.R
+import com.example.intersvyaztestapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navController = (supportFragmentManager.findFragmentById(R.id.fcv) as NavHostFragment).navController
+        NavigationUI.setupWithNavController(binding.bnv, navController)
     }
 }
